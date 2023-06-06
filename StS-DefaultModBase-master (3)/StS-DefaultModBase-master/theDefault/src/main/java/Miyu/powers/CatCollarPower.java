789140c289b2b4a4cc268bb5c88165f0b81b55f3
TwitchCbrace <135733377+TwitchCbrace@users.noreply.github.com>
2023-06-07 02:28:49 +0900

    이상한데서작업함
    ㅇㅇ

--- StS-DefaultModBase-master (3)/StS-DefaultModBase-master/theDefault/src/main/java/Miyu/powers/CatCollarPower.java
+++ StS-DefaultModBase-master (3)/StS-DefaultModBase-master/theDefault/src/main/java/Miyu/powers/CatCollarPower.java
@@ -1,45 +1,34 @@
 package Miyu.powers;
 
 import Miyu.DefaultMod;
-import Miyu.cards.ICoverCard;
-import Miyu.cards.OnReduceCover;
-import Miyu.cards.Rock;
+import Miyu.cards.OnReduceTrash;
 import Miyu.util.TextureLoader;
 import basemod.interfaces.CloneablePowerInterface;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.GainBlockAction;
-import com.megacrit.cardcrawl.actions.common.MakeTempCardInHandAction;
-import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.cards.tempCards.Shiv;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
-import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
-import com.megacrit.cardcrawl.powers.watcher.VigorPower;
 
 import static Miyu.DefaultMod.makePowerPath;
 
+//Gain 1 dex for the turn for each card played.
 
-public class ContestedAreaPower extends AbstractPower implements CloneablePowerInterface {
+public class CatCollarPower extends AbstractPower implements CloneablePowerInterface {
     public AbstractCreature source;
 
-    public static final String POWER_ID = DefaultMod.makeID("ContestedAreaPower");
+    public static final String POWER_ID = DefaultMod.makeID("CatCollarPower");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
 
     // We create 2 new textures *Using This Specific Texture Loader* - an 84x84 image and a 32x32 one.
     // There's a fallback "missing texture" image, so the game shouldn't crash if you accidentally put a non-existent file.
-    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("ContestedAreaPower84.png"));
-    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("ContestedAreaPower32.png"));
+    private static final Texture tex84 = TextureLoader.getTexture(makePowerPath("CatCollarPower84.png"));
+    private static final Texture tex32 = TextureLoader.getTexture(makePowerPath("CatCollarPower32.png"));
 
-    public ContestedAreaPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
+    public CatCollarPower(final AbstractCreature owner, final AbstractCreature source, final int amount) {
         name = NAME;
         ID = POWER_ID;
 
@@ -49,7 +38,6 @@ public class ContestedAreaPower extends AbstractPower implements CloneablePowerI
 
         type = PowerType.BUFF;
         isTurnBased = false;
-        isPostActionPower = true;
 
         // We load those txtures here.
         this.region128 = new TextureAtlas.AtlasRegion(tex84, 0, 0, 84, 84);
@@ -57,15 +45,18 @@ public class ContestedAreaPower extends AbstractPower implements CloneablePowerI
 
         updateDescription();
     }
+    @Override
+    public void reducePower (int reduceAmount) {
+        if (this.amount - reduceAmount <= 0) { this.fontScale = 8.0F;
+            reduceAmount -= this.amount; this.amount = 0;
+        } else {
+            this.fontScale = 8.0F; this.amount -= reduceAmount;
+        } for ( AbstractPower p : owner.powers )
+        { if ( p instanceof OnReduceTrash) { ((OnReduceTrash) p).trashReduced(reduceAmount); } } }
 
-    public void onExhaust(AbstractCard card) {
-        if (card instanceof ICoverCard) {
-            this.flash();
-            addToBot(new ApplyPowerAction(owner, owner,
-                    new HandSizeUp(owner, owner, amount), amount));
-            addToBot(new MakeTempCardInHandAction(new Rock(), amount));
-        }
-    }
+    @Override
+    public void onRemove() { for ( AbstractPower p : owner.powers )
+    { if ( p instanceof OnReduceTrash ) { ((OnReduceTrash) p).trashReduced(amount); } } }
 
     @Override
     public void updateDescription() {
@@ -78,8 +69,6 @@ public class ContestedAreaPower extends AbstractPower implements CloneablePowerI
 
     @Override
     public AbstractPower makeCopy() {
-        return new ContestedAreaPower(owner, source, amount);
+        return new CatCollarPower(owner, source, amount);
     }
-
-
 }
