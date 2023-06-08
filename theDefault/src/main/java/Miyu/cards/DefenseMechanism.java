48785b1167300517e2ffd243704232db273214d0
TwitchCbrace <135733377+TwitchCbrace@users.noreply.github.com>
2023-06-09 03:28:29 +0900

    방어기제 카드 추가

---
+++ theDefault/src/main/java/Miyu/cards/DefenseMechanism.java
@@ -0,0 +1,57 @@
+package Miyu.cards;
+
+import Miyu.DefaultMod;
+import Miyu.characters.TheDefault;
+import Miyu.powers.CleanUpTrashPower;
+import Miyu.powers.DefenseMechanismPower;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.CardStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Miyu.DefaultMod.makeCardPath;
+
+public class DefenseMechanism extends AbstractDynamicCard {
+
+    public static final String ID = DefaultMod.makeID(DefenseMechanism.class.getSimpleName());
+    public static final String IMG = makeCardPath("DefenseMechanism.png");
+    private static final CardStrings cardStrings = CardCrawlGame.languagePack.getCardStrings(ID);
+    public static final String UPGRADE_DESCRIPTION = cardStrings.UPGRADE_DESCRIPTION;
+    // /TEXT DECLARATION/
+
+
+    // STAT DECLARATION
+
+    private static final CardRarity RARITY = CardRarity.UNCOMMON;
+    private static final CardTarget TARGET = CardTarget.SELF;
+    private static final CardType TYPE = CardType.POWER;
+    public static final CardColor COLOR = TheDefault.Enums.COLOR_GRAY;
+
+    private static final int COST = 1;
+    private static final int MAGIC = 1;
+
+    public DefenseMechanism() {
+        super(ID, IMG, COST, TYPE, COLOR, RARITY, TARGET);
+        this.baseMagicNumber = this.magicNumber = MAGIC;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        AbstractDungeon.actionManager.addToBottom(new ApplyPowerAction(p, p,
+                new DefenseMechanismPower(p, p, magicNumber), magicNumber)
+        );
+    }
+
+
+    @Override
+    public void upgrade() {
+        if (!upgraded) {
+            upgradeName();
+            rawDescription = UPGRADE_DESCRIPTION;
+            isInnate = true;
+            initializeDescription();
+        }
+    }
+}
