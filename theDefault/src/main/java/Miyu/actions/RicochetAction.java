4ac93a4df6df592a685213124242757b21ff5603
HaecheonKim <code.bug.station@gmail.com>
2023-06-22 15:40:23 +0900

    Fix typos

--- theDefault/src/main/java/Miyu/actions/RicochetAction.java
+++ theDefault/src/main/java/Miyu/actions/RicochetAction.java
@@ -26,23 +26,20 @@ public class RicochetAction extends AbstractGameAction {
 	}
 
 	public void update() {
-		/*
-		* 아래 로직은 엄밀히 말하자면, `카드를 시행하면, 그 순간 존재하는 모든 적의 위치를 타격하면 멈춘다.` 에 가깝습니다.
-			* 그래서 아래 가정 2개가 깨질 경우, 버그가 생길 수 있습니다.
-				* 가정 1: 나의 공격 턴 중에 몬스터가 분열하거나 위치를 바꾸지 않는다.
-				* 가정 2: 1개의 도탄 공격이 시행 중이다.
-					* 이 때 도탄 공격에 의해 몬스터가 죽은 경우, 바로 그 자리에 다른 개체가 생성되어 다음 도탄 공격들의 대상이 되지 않는다.
+		// * 아래 로직은 엄밀히 말하자면, `카드를 시행하면, 그 순간 존재하는 모든 적의 위치를 타격하면 멈춘다.` 에 가깝습니다.
+		// * 그래서 아래 가정 2개가 깨질 경우, 버그가 생길 수 있습니다.
+		// * 가정 1: 나의 공격 턴 중에 몬스터가 분열하거나 위치를 바꾸지 않는다.
+		// * 가정 2: 1개의 도탄 공격이 시행 중일 때 몬스터가 죽은 경우, 바로 그 자리에 다른 개체가 생성되어 다음 도탄 공격들의 대상이 되지 않는다.
 
-		* 부메랑 칼날, 탄성 플라스크의 적 선택 메커니즘을 사용하지 않은 이유
-			* `AbstractDungeon.getMonsters().getRandomMonster((AbstractMonster)null, true, AbstractDungeon.cardRandomRng);`
-			* 위 함수는 `무작위 적`을 선택하는 함수로서, `특정 적을 때렸는가?`를 파악하기에 적절하지 않습니다.
+		// * 부메랑 칼날, 탄성 플라스크의 적 선택 메커니즘을 사용하지 않은 이유
+		// * `AbstractDungeon.getMonsters().getRandomMonster((AbstractMonster)null, true,
+		// AbstractDungeon.cardRandomRng);`
+		// * 위 함수는 `무작위 적`을 선택하는 함수로서, `특정 적을 때렸는가?`를 파악하기에 적절하지 않습니다.
 
-		* 참고: id 만으로 몇 번째 몬스터인지 파악할 수 없음에 주의하세요.
-			* `fight 4_Byrd` 의 경우, 4개의 섀가 등장하는데,
-				* 이 4개의 섀는 모두 `AbstractMonster.id==Byrd`, `AbstractMonster.name==섀` 값을 가지고 있습니다.
-			* 즉, stringID만 가지고 몇 번째 섀를 골라 낼 수 있는 방법은 아직 제대로 파악하지 못했습니다.
-		*/
-
+		// * 참고: id 만으로 몇 번째 몬스터인지 파악할 수 없음에 주의하세요.
+		// * `fight 4_Byrd` 의 경우, 4개의 섀가 등장하는데,
+		// * 이 4개의 섀는 모두 `AbstractMonster.id==Byrd`, `AbstractMonster.name==섀` 값을 가지고 있습니다.
+		// * 즉, stringID만 가지고 몇 번째 섀를 골라 낼 수 있는 방법은 아직 제대로 파악하지 못했습니다.
 
 		// 몬스터가 존재할 수 있는 위치들을 가져옵니다
 		ArrayList<AbstractMonster> monsters = AbstractDungeon.getMonsters().monsters;
